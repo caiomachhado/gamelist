95e67d4a62eaecd5608b73cdc030788ad60b1103
Caio Machado <caioamsul@gmail.com>
2021-04-17 23:52:07 -0300

    Insert Layout

---
+++ app/src/main/java/com/example/gamelist/adapter/GamesAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.gamelist.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GamesAdapter {
+
+    class GameViewHolder(itemView: View){
+
+        
+
+    }
+
+}
